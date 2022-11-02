a3f42640492ba66b824bbf235032c39ebb5ad018
Melowcode <107577736+Melowcode@users.noreply.github.com>
2022-11-02 18:22:48 +0100

    adding restart game func

--- app.js
+++ app.js
@@ -1,74 +1,59 @@
-const choice = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+const start = document.querySelector(".start");
+const playerScoreBoard = document.querySelector("#playerScore");
+const computerScoreBoard = document.querySelector("#ComputerScore");
+const playerOption = [...document.querySelectorAll(".player-button")];
+const sayHand = document.querySelector("#sayHand");
+const choices = ["rock", "paper", "scissors"];
+const para = document.querySelector("#para");
+// On click start game
 
-function getComputerChoice() {
-  const computerChoice = choice[Math.floor(Math.random() * choice.length)];
+start.addEventListener("click", (e) => {
+  e.stopPropagation();
 
-  return computerChoice;
-}
-
-function checkwiner(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "tie";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "scissor" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "rock")
-  ) {
-    return "player";
-  } else {
-    return "computer";
-  }
-}
-
-function playRound(playerSelection, computerSelection) {
-  const winer = checkwiner(playerSelection, computerSelection);
-  if (winer === "tie") {
-    return "it's a tie!";
-  } else if (winer === "player") {
-    return `Player Won ${playerSelection} beats ${computerSelection}`;
-  } else {
-    return `computer Won ${computerSelection} beats ${playerSelection}`;
-  }
-}
-
-function getplayerChoice() {
-  let validateInput = false;
-  while (validateInput === false) {
-    const select = prompt(choice);
-    if (select === null) {
-      continue;
-    }
-    const selectInlower = select.toLowerCase();
-
-    if (choice.includes(selectInlower)) {
-      validateInput = true;
-      return selectInlower;
-    }
-  }
-}
+  start.classList.add("fadeout");
+});
 
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = getplayerChoice();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
+  // get player choice on click
+  playerOption.forEach((option) => {
+    option.addEventListener("click", (e) => {
+      e.stopPropagation();
+      const playerOption = e.target.closest(".player-button");
+      const playerChoice = playerOption.id;
+      // get computer choice
+      const computerChoice =
+        choices[Math.floor(Math.random() * choices.length)];
+      console.log(playerChoice);
+      console.log(computerChoice);
+      // compare player choice and computer choice
 
-    if (checkwiner(playerSelection, computerSelection) === "player") {
-      playerScore++;
-    } else if (checkwiner(playerSelection, computerSelection) === "computer") {
-      computerScore++;
-    }
-  }
-  if (playerScore > computerScore) {
-    console.log("Player Win");
-  } else if (playerScore < computerScore) {
-    console.log("Computer Win");
-  } else {
-    console.log("its a tie");
-  }
+      if (playerChoice === computerChoice) {
+        sayHand.innerText = "its a tie";
+      } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+      ) {
+        sayHand.innerText = `Player Won ${playerChoice} beats ${computerChoice}`;
+        playerScore += 1;
+        playerScoreBoard.textContent = playerScore;
+      } else {
+        sayHand.innerText = `Computer Won ${computerChoice} beats ${playerChoice}`;
+        computerScore += 1;
+        computerScoreBoard.textContent = computerScore;
+      }
+
+      if (playerScore === 5) {
+        para.textContent = "Player Win";
+
+        start.classList.remove("fadeout");
+      } else if (computerScore === 5) {
+        start.classList.remove("fadeout");
+        para.textContent = "Computer Win";
+      }
+    });
+  });
 }
 game();
-
-document.querySelector("body").style.backgroundColor = "black";
